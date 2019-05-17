9e5f72f1623eadbc28ba39c265cbdd665cbff2dc
ulmers <stephenulmer@knights.ucf.edu>
2019-05-17 18:11:40 -0400

    Heroku Deployment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'HealthInformationApp'`, () => {
+  it(`should have as title 'IronPidginApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('HealthInformationApp');
+    expect(app.title).toEqual('IronPidginApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to HealthInformationApp!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to IronPidginApp!');
   });
 });
